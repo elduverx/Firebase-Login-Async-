bd8c43d22de1110f66ee792a64f89faf18dac0ae
elduverx <duverwild@gmail.com>
2023-07-06 14:00:29 +0200

    Initial Commit

---
+++ FireLogin/FireLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireLoginApp.swift
+//  FireLogin
+//
+//  Created by Duver on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FireLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
